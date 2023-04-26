d959910a2a7fd4c145887a52d2fb1814a49cf548
Venanciofaf <venanciofaf@al.insper.edu.br>
2023-04-26 14:33:17 -0300

    Commit exercicio 1

--- dev2.py
+++ dev2.py
@@ -1,1 +1,10 @@
-print("Primeiro commit do Venâncio Freitas")
+def define_posicoes(linha , coluna , orientaçao , tamanho): 
+    posição = []
+    for numero in range(tamanho): 
+        if orientaçao == 'horizontal': 
+            posição.append([linha , coluna+numero])
+        if orientaçao == 'vertical': 
+            posição.append([linha+numero , coluna)
+    
+    return posição
+print(define_posicoes(2, 4,'vertical', 3))
